bb6a9a19da2a9e02f69303017cf39f67c191f491
mozarist <azzammozarist.xpro@gmail.com>
2025-10-06 08:54:27 +0700

    profile ui update

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-indigo-700']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-rose-600']) }}>
     {{ $value ?? $slot }}
 </label>
